323218f1f1edf608b1cbdc9518b5a133ec7e04d4
venderizza <karinad2016god@gmail.com>
2026-03-17 01:11:10 +0400

    done

--- app/internal/database/dbconnect.go
+++ app/internal/database/dbconnect.go
@@ -1,9 +1,12 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"time"
 
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
 	"lab-1/internal/models"
@@ -12,14 +15,41 @@ import (
 var DB *gorm.DB
 
 func Connect() {
+	host := getEnv("DB_HOST", "localhost")
+	port := getEnv("DB_PORT", "5432")
+	user := getEnv("DB_USER", "postgres")
+	password := getEnv("DB_PASSWORD", "")
+	dbname := getEnv("DB_NAME", "tasks")
+	sslmode := getEnv("DB_SSLMODE", "disable")
+	timezone := getEnv("DB_TIMEZONE", "UTC")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		host, user, password, dbname, port, sslmode, timezone,
+	)
+
 	var err error
-	DB, err = gorm.Open(sqlite.Open("tasks.db"), &gorm.Config{})
+	for i := 0; i < 10; i++ {
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		log.Printf("Postgres not ready, retrying in 3s... (%d/10): %v", i+1, err)
+		time.Sleep(3 * time.Second)
+	}
+
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
-	err = DB.AutoMigrate(&models.Task{})
-	if err != nil {
+	if err := DB.AutoMigrate(&models.Task{}); err != nil {
 		log.Fatal("Failed to migrate database:", err)
 	}
+}
+
+func getEnv(key string, fallback string) string {
+	if v, ok := os.LookupEnv(key); ok && v != "" {
+		return v
+	}
+	return fallback
 }
